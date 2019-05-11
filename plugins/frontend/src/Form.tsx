850abb64f88fe41686f696c16de0fe83a4dba9c1
uitspitss <uitspitss@gmail.com>
2019-05-11 17:27:57 +0900

    Fix polling

--- plugins/frontend/src/Form.tsx
+++ plugins/frontend/src/Form.tsx
@@ -55,86 +55,93 @@ const Form: React.FC<InjectedFormikProps<{}, FormValues>> = ({
       query={CHECK_PLAYLIST_URL}
       variables={{ url: encodeURIComponent(values.url) }}
       skip={!isValid}
-      pollInterval={1000}
     >
-      {({ data, loading }) => (
-        <Grid
-          container
-          direction="column"
-          justify="space-between"
-          alignItems="center"
-        >
-          <Grid item xs={9}>
-            <Mutation mutation={POST_PLAYLIST_URL} key="post">
-              {postPlaylist => (
-                <form
-                  encType="multipart/form-data"
-                  onSubmit={(e: any) => {
-                    e.preventDefault();
-                    postPlaylist({ variables: { url: values.url } });
-                  }}
-                >
-                  <Grid container direction="row" justify="center">
-                    <Grid item style={{ margin: 10 }}>
-                      <TextField
-                        name="url"
-                        value={values.url}
-                        onChange={handleChange}
-                        label="shorten playlist url"
-                        placeholder="shorten url"
-                        style={{ width: '50vw' }}
-                      />
+      {({ data, loading, startPolling, stopPolling }) => {
+        if (isValid) {
+          startPolling(1000);
+        } else {
+          stopPolling();
+        }
+
+        return (
+          <Grid
+            container
+            direction="column"
+            justify="space-between"
+            alignItems="center"
+          >
+            <Grid item xs={9}>
+              <Mutation mutation={POST_PLAYLIST_URL} key="post">
+                {postPlaylist => (
+                  <form
+                    encType="multipart/form-data"
+                    onSubmit={(e: any) => {
+                      e.preventDefault();
+                      postPlaylist({ variables: { url: values.url } });
+                    }}
+                  >
+                    <Grid container direction="row" justify="center">
+                      <Grid item style={{ margin: 10 }}>
+                        <TextField
+                          name="url"
+                          value={values.url}
+                          onChange={handleChange}
+                          label="shorten playlist url"
+                          placeholder="shorten url"
+                          style={{ width: '50vw' }}
+                        />
+                      </Grid>
+                      <Grid item style={{ margin: 10 }}>
+                        {loading ? (
+                          <CircularProgress />
+                        ) : (
+                          <Button
+                            type="submit"
+                            variant="contained"
+                            disabled={!isValid || isSubmitting}
+                            color="primary"
+                          >
+                            {isSubmitting ? 'submitting...' : 'shorten'}
+                          </Button>
+                        )}
+                      </Grid>
                     </Grid>
-                    <Grid item style={{ margin: 10 }}>
-                      {loading ? (
-                        <CircularProgress />
-                      ) : (
-                        <Button
-                          type="submit"
-                          variant="contained"
-                          disabled={!isValid || isSubmitting}
-                          color="primary"
-                        >
-                          {isSubmitting ? 'submitting...' : 'shorten'}
-                        </Button>
-                      )}
-                    </Grid>
-                  </Grid>
-                </form>
-              )}
-            </Mutation>
+                  </form>
+                )}
+              </Mutation>
+            </Grid>
+            <Grid item xs={6} style={{ margin: 10 }}>
+              <TextField
+                label="shorten playlist url"
+                value={
+                  data && data.playlists && data.playlists.length
+                    ? `${location.protocol}//${location.host}/${
+                        data.playlists[0].short_id
+                      }`
+                    : ''
+                }
+                placeholder="shorten url"
+                style={{ width: '30vw' }}
+                InputLabelProps={{ shrink: true }}
+              />
+            </Grid>
+            <Grid item xs={6} style={{ margin: 20 }}>
+              {data &&
+                data.playlists &&
+                data.playlists.map((pl: any) => (
+                  <GridList cols={1} key="shorten">
+                    <GridListTile key={`shorten_${pl.short_id}`}>
+                      <img src={pl.thumbnail_url} />
+                      <a href={`${pl.short_id}`}>
+                        <GridListTileBar title={pl.title} titlePosition="top" />
+                      </a>
+                    </GridListTile>
+                  </GridList>
+                ))}
+            </Grid>
           </Grid>
-          <Grid item xs={6} style={{ margin: 10 }}>
-            <TextField
-              label="shorten playlist url"
-              value={
-                data && data.playlists && data.playlists.length
-                  ? `${location.protocol}//${location.host}/${
-                      data.playlists[0].short_id
-                    }`
-                  : ''
-              }
-              placeholder="shorten url"
-              style={{ width: '30vw' }}
-              InputLabelProps={{ shrink: true }}
-            />
-          </Grid>
-          <Grid item xs={6} style={{ margin: 20 }}>
-            {data &&
-              data.playlists &&
-              data.playlists.map((pl: any) => (
-                <GridList cols={1} key="shorten">
-                  <GridListTile key={`shorten_${pl.short_id}`}>
-                    <img src={pl.thumbnail_url} />
-                    <a href={`${pl.short_id}`}>
-                      <GridListTileBar title={pl.title} titlePosition="top" />
-                    </a>
-                  </GridListTile>
-                </GridList>
-              ))}
-          </Grid>
-        </Grid>
-      )}
+        );
+      }}
     </Query>
   );
 };
